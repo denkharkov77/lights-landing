b3e3e604f4b9c5a1e903e291616c2191aa5d033f
Denkharkov77 <Denkharkov77@ukr.net>
2026-06-27 21:09:24 +0300

    update index.php

--- index.php
+++ index.php
@@ -46,12 +46,17 @@
     include 'blocks/gallery.php';
     include 'blocks/video.php';
     include 'blocks/prices.php';
+    include 'blocks/box.php';
+    include 'blocks/comparison.php';
+    include 'blocks/faq.php';
+    include 'blocks/quiz.php';
+    include 'blocks/anatomy.php';
     include 'blocks/reviews.php';
     include 'blocks/order.php';
     include 'blocks/footer.php';
     ?>
 
     <!-- Подключаем JavaScript для анимации -->
-    <script src="assets/js/script.js"></script>
+    <script defer src="assets/js/script.js"></script>
 </body>
 </html>
